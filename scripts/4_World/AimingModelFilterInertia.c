d645edd57f7e5c1f97d96540b4fa9c78bd29872c
simonvic <simonvic.dev@gmail.com>
2022-09-20 17:42:33 +0200

    fix: bipod not reducing inertia

--- scripts/4_World/AimingModelFilterInertia.c
+++ scripts/4_World/AimingModelFilterInertia.c
@@ -2,7 +2,7 @@ modded class AimingModelFilterInertia {
 	override float computeInertiaMultiplier() {
 		float m = super.computeInertiaMultiplier();
 		if (getWeapon().IsBipodDeployed() && getWeapon().Bipod().IsBipodInUse()) {
-			return 1;
+			return 0;
 		}
 		return m;
 	}
